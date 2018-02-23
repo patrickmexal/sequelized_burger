ffbf7390f1282c37a6f9950de55b819b2689ebc6
Patrick Mexal <patrickmexal@gmail.com>
2018-02-23 15:11:29 -0700

    update twate twelve

--- app/models/burger.js
+++ app/models/burger.js
@@ -1,13 +1,13 @@
-// Sequelize (capital) references the standard library
+
 var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
+// sequelize (low-case) references our connection to the DB.
 var sequelize = require("../config/connection.js");
 
-// Creates a "Character" model that matches up with DB
+
 var burgers = sequelize.define("burgers", {
-  // the routeName gets saved as a string
+ 
   id: Sequelize.INTEGER,
-  // the name of the burger
+
   burger_name: Sequelize.STRING,
   // burger devoured?
   devoured: Sequelize.BOOLEAN
